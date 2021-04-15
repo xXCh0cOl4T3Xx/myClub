0b35b1ff8c114770c646d4a18e075103009573fc
José Claudio Alves <alvesjclaudio@hotmail.com>
2021-04-14 23:30:54 -0300

    Criado banco User

---
+++ db/migrate/20210415023007_create_users.rb
@@ -0,0 +1,26 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.integer :kind
+      t.integer :position
+      t.integer :status
+      t.date :year
+      t.date :marital
+      t.string :phone
+      t.string :emergency_phone
+      t.string :email
+      t.date :date_add
+      t.date :date_baptized
+      t.string :occupation
+      t.string :function
+      t.text :notes
+      t.date :birth
+      t.string :cpf
+      t.string :photo
+      t.references :faction, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
