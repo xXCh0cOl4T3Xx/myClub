0dacca668f63a450c7a282f1fdf6ab47d35f255e
José Claudio Alves <alvesjclaudio@hotmail.com>
2021-04-14 23:32:06 -0300

    Gerado migrate db user

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_04_15_023007) do
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.integer "kind"
+    t.integer "position"
+    t.integer "status"
+    t.date "year"
+    t.date "marital"
+    t.string "phone"
+    t.string "emergency_phone"
+    t.string "email"
+    t.date "date_add"
+    t.date "date_baptized"
+    t.string "occupation"
+    t.string "function"
+    t.text "notes"
+    t.date "birth"
+    t.string "cpf"
+    t.string "photo"
+    t.integer "faction_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["faction_id"], name: "index_users_on_faction_id"
+  end
+
+  add_foreign_key "users", "factions"
 end
